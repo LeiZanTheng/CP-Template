9e4750e69f0204a95677782d73edfc33162e6c85
LeiZanThengVNGuy <thangthangsieupham@gmail.com>
2024-06-07 11:01:59 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#define FASTER cin.tie(0)->ios_base::sync_with_stdio(0);
+#define USEFILE freopen("INPUT.INP", "r", stdin); freopen("OUTPUT.OUT", "w", stdout);
+using namespace std;
+int main(){FASTER;
+    USEFILE
+
+}
